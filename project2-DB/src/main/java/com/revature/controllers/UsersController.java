4355c70397d611ebcfba1684613abbe0315cc5ea
jbb111116 <jon.bed@hotmail.com>
2018-11-26 14:06:40 -0600

    New changes

--- project2-DB/src/main/java/com/revature/controllers/UsersController.java
+++
@@ -1,8 +0,0 @@
-package com.revature.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UsersController {
-
-}
--- project2-DB/src/main/java/com/revature/services/UserServices.java
+++
@@ -1,9 +0,0 @@
-package com.revature.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServices {
-	
-
-}
